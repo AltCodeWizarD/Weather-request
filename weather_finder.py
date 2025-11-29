0a414df0041c9d8b9ec26533e6002c48e7da5a72
AltCodeWizarD <rusich2@gmail.com>
2025-11-29 13:25:29 +0300

    Глобальный апдейт
    1. Высушил код, убрал мусорные принты
    2. Переделал код для импорта его как модуль
    3. Перенес проверку try-except в if-main
    4. Дал переменным верные названия
    5. Убрал из констант лишнее

--- weather_finder.py
+++ weather_finder.py
@@ -1,48 +1,48 @@
 import requests
 
 BASE_URL = "https://wttr.in/"
-LOCATIONS = ["london", "Cherepovec", "SVO"]
-
-PARAMS = {
-    "nTqu": "",
-    "lang": "en",
-    "m": "",
-    "n": "",
-}
 
 
-def fetch_single_weather(location):
-    """Получает погоду для одной локации"""
+def fetch_single_weather(location, params): 
     url = f"{BASE_URL}{location}"
-    PARAMS["lang"] = "ru"
 
     try:
-        response = requests.get(url, params=PARAMS)
+        response = requests.get(url, params=params)
         response.raise_for_status()
-        return response.text, response.status_code
+        return response.text
     except requests.exceptions.HTTPError as http_error:
-        return f"HTTP ошибка: {http_error}", None
+        return f"HTTP ошибка: {http_error}"
     except requests.exceptions.ConnectionError as connection_error:
-        return f"Ошибка подключения: {connection_error}", None
+        return f"Ошибка подключения: {connection_error}"
     except requests.exceptions.Timeout as timeout_error:
-        return f"Таймаут: {timeout_error}", None
+        return f"Таймаут: {timeout_error}"
     except requests.exceptions.RequestException as request_error:
-        return f"Произошла ошибка: {request_error}", None
+        return f"Произошла ошибка: {request_error}"
 
 
-def print_weather_info(location, weather_data, status_code):
-    if status_code:
-        print(f"\nПогода в {location}")
-        print(f"Статус запроса: {status_code}")
-        print(f"Прогноз погоды:\n{weather_data}")
+def print_weather_info(location, result):
+    error_names = ["HTTP ошибка", "Ошибка подключения", "Таймаут", "Произошла ошибка"]
+    
+    if any(result.startswith(error_name) for error_name in error_names):
+        print(f"\nОшибка для локации {location}:\n{result}")
     else:
-        print(f"\nОшибка для локации {location}:\n{weather_data}")
+        print(f"Прогноз погоды:\n{result}")
 
 
 def main():
-    for location in LOCATIONS:
-        weather_data, status_code = fetch_single_weather(location)
-        print_weather_info(location, weather_data, status_code)
+    locations_to_check = ["london", "Cherepovec", "SVO"]
+    request_params = {
+        "nTqu": "",
+        "lang": "ru",
+        "m": "",
+        "n": "",
+    }
+    try:
+        for location in locations_to_check:
+            weather_data = fetch_single_weather(location, request_params)
+            print_weather_info(location, weather_data)
+    except Exception as exs:
+        print(f"Произошла непредвиденная ошибка: {exs}")
 
 
 if __name__ == "__main__":
